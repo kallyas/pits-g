fc50b0cbed3ee5a1d9581eb317be22b357d7997d
kallyas <kallyasmedia@gmail.com>
2022-07-16 14:58:11 +0300

    ch: remake

--- src/style/animations.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import { keyframes } from '@emotion/react/macro'
-
-export const cardUp = keyframes`
-  from {transform: translateY(60px); opacity: 0;}
-  to { transform: translateY(0); opacity: 1;}
-`
--- src/style/variables.js
+++
@@ -1,3 +0,0 @@
-export const headerHeight = 64
-export const headerHeightTablet = 105
-export const headerHeightFull = 84
